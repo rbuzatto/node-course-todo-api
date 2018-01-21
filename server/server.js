1f861acae5808b23589630827fc3d49bf428aea4
rafael bc <mightyrafa@gmail.com>
2018-01-21 02:21:50 -0200

    server.js update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 
-var{mongoose}   = require('./db/mongoose');
+var {mongoose}  = require('./db/mongoose');
 var {Todo}      = require('./models/todo');
 var {User}      = require('./models/user');
 
