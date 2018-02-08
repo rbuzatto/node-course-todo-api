70ccd34add401a34938e27d751d7ef4a9f3fc119
rafael bc <mightyrafa@gmail.com>
2018-02-08 02:09:36 -0200

    Add GET /todot/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
+var {ObjectID}  = require('mongodb');
 
 var {mongoose}  = require('./db/mongoose');
 var {Todo}      = require('./models/todo');
@@ -33,12 +34,33 @@ app.post('/users', (req, res) => {
     });
 });
 
-app.get('/todos',(req, res) => {
+app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos}); //aqui ele envia obj, pq no futuro vc pode inserir outros itens
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET /todos/12345
+// validate id -> ObjectID method - respond w/ 404 e send empty body
+// querie data w/ findById
+// success -> if todo - send it back / if no todo - send back 404
+// error -> send back 400 - não enviamos de volta nada no momento pq o error pode enviar private info
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo}); //caso queira enviar outras infos junto do obj
+
+    }).catch((e) => res.status(400).send());
 });
 
 
