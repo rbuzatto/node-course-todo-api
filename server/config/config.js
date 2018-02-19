9f7fb0a1f06c4d54aa28bacf52b6e77199657e1e
rafael bc <mightyrafa@gmail.com>
2018-02-19 15:27:53 -0300

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,21 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGO_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGO_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+    let config = require('./config.json');
+    let envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
-if (process.env.NODE_ENV === 'production') {
-    process.env.MONGO_URI = 'mongodb://dumbuser:test123@ds229648.mlab.com:29648/nodecourse_todoapp';
-}
+
+// if (env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGO_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//     process.env.PORT = 3000;
+//     process.env.MONGO_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
+// if (process.env.NODE_ENV === 'production') {
+//     process.env.MONGO_URI = 'mongodb://dumbuser:test123@ds229648.mlab.com:29648/nodecourse_todoapp';
+// }
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports ={mongoose};
