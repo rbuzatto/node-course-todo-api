9aac7a6178082e6e89c4cac8241d8f2f4f52c49f
rafael bc <mightyrafa@gmail.com>
2018-02-19 15:35:52 -0300

    Edit config json

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,3 @@ if (env === 'development' || env === 'test') {
 //     process.env.PORT = 3000;
 //     process.env.MONGO_URI = 'mongodb://localhost:27017/TodoAppTest';
 // }
-// if (process.env.NODE_ENV === 'production') {
-//     process.env.MONGO_URI = 'mongodb://dumbuser:test123@ds229648.mlab.com:29648/nodecourse_todoapp';
-// }
